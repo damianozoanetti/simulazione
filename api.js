5ae08fe708813a3d6f2780472c51dc855b867025
Enrico <enrico.berthod@studenti.unitn.it>
2018-12-05 11:57:01 +0100

    funzioni ok

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ app.use( bodyParser.json() )
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8000
-//const url = 'https://simulazione.herokuapp.com'
 const url = 'http://localhost:8000'
 
 const string_square = require('./string_square').string_square
@@ -19,6 +18,8 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/square', (req, res) => {
     var x = string_square(req.query.string);
+    var risultato = {result: x};
+    res.send(risultato);
 })
 
 //?string=my_string
--- client.js
+++ client.js
@@ -1,13 +1,12 @@
 var https = require('http');
 const fetch = require("node-fetch");
 
-//const url = 'https://simulazione.herokuapp.com'
 const url = 'http://localhost:8000'
 
 async function get(url) {
 	console.log('\n\ngetting ' + url + '\n')
     try {
-		const response = await fetch(url);
+	const response = await fetch(url);
         const json = await response.json();
         console.log(json)
     } catch (error) {
@@ -17,4 +16,4 @@ async function get(url) {
 
 var my_string = 'prova';
 
-get(url+'/square?string=my_string');
+get(url+'/square?string='+my_string);
